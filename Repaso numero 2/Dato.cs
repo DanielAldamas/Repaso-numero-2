5231d91e0792834f211e4b7e82e72e53ae595bf4
ƬψƬ ☢ Sirius√ <danieloalda11@outlook.com>
2020-05-10 17:13:18 -0600

    Agregar archivos de proyecto.

---
+++ Repaso numero 2/Dato.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso_numero_2
+{
+    class Dato
+    {
+        string nit;
+        string nombre;
+        string direccion;
+        string placa;
+        string fechaA;
+        string fechaV;
+        Double total;
+        Double kilometro;
+
+        public string Nit { get => nit; set => nit = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public string Placa { get => placa; set => placa = value; }
+        public string FechaA { get => fechaA; set => fechaA = value; }
+        public double Total { get => total; set => total = value; }
+        public double Kilometro { get => kilometro; set => kilometro = value; }
+    }
+}
---
+++ Repaso numero 2/Vehiculo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso_numero_2
+{
+    class Vehiculo
+    {
+        string placa;
+        string marca;
+        string modelo;
+        string color;
+        Double precio;
+
+        public string Placa { get => placa; set => placa = value; }
+        public string Marca { get => marca; set => marca = value; }
+        public string Modelo { get => modelo; set => modelo = value; }
+        public string Color { get => color; set => color = value; }
+        public double Precio { get => precio; set => precio = value; }
+    }
+}
